5d1b70a1bc2ba0ed7a145ffd4db9a3d0a1cce381
Harsh5itha <45786603+Harsh5itha@users.noreply.github.com>
2021-10-19 04:09:22 +0530

    V2

--- app.js
+++ app.js
@@ -5,48 +5,34 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const app = express();
+var items = ["Buy","Cook","Eat"];
 
 app.set("view engine", "ejs");
 
-// app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.get ("/", function(req, res){
   // res.sendFile(__dirname+"/signup.html");
 
   var today = new Date();
-  var cuurndday =  today.getDate();
-  var day = "";
-  switch (cuurndday) {
-    case 0:
-      day = "Sunday";
-    break;
-    case 1:
-      day = "Monday";
-    break;
-    case 2:
-      day = "Tuesday";
-    break;
-    case 3:
-      day = "Wednesday";
-    break;
-    case 4:
-      day = "Thursday";
-    break;
-    case 5:
-      day = "Friday";
-    break;
-    case 6:
-      day = "Saturday";
-    break;
-    default:
-    console.log("Error current day is: "+cuurndday);
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
-  }
   res.render("list", {
-    kindOfday: day
+    kindOfday: day,
+    newlistitems: items
   });
 });
 
+app.post("/", function(req, res){
+  var item = req.body.newitem;
+  items.push(item);
+  res.redirect("/");
+});
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
